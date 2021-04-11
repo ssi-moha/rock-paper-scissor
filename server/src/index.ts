6cedd49692fe079513009fa0b0225c58418f8901
Sinane <sin.sim@outlook.fr>
2021-04-11 11:46:53 +0200

    server: init socket-io and express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,20 @@
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = "Server running...";
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import express from "express";
+import socketIO from "socket.io";
+import http from "http";
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+const message = "Server running on localhost:3000";
+
+app.get("/", (req, res) => {
+  res.send(message);
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(3000, () => {
+  console.log(message);
+});
--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,23 +1,35 @@
-const path = require('path');
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const WebpackShellPlugin = require("webpack-shell-plugin-next");
 
 module.exports = {
-  entry: './src/index.ts',
- devtool: 'inline-source-map',
+  entry: "./src/index.ts",
+  target: "node",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.ts?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [ '.ts', '.js' ],
+    extensions: [".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new WebpackShellPlugin({
+      onBuildEnd: {
+        scripts: ["yarn run:dev"],
+        blocking: false,
+        parallel: true,
+      },
+    }),
+  ],
+  externals: [nodeExternals()],
 };
-
